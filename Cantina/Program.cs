7f0fd1d85e79e7b2e859d4babc5d8ca557884b40
lndsilva <lndsilva@hotmail.com>
2024-08-30 21:48:02 -0300

    funcionarios

--- Cantina/Program.cs
+++ Cantina/Program.cs
@@ -16,7 +16,7 @@ namespace Cantina
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMenuPrincipal());
+            Application.Run(new frmFuncionarios());
         }
     }
 }

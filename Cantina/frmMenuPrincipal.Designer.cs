1a49e9dfa35ca5390b4bfa4a4a75621aa456092c
lndsilva <lndsilva@hotmail.com>
2024-08-30 21:29:10 -0300

    menu principal

--- Cantina/frmMenuPrincipal.Designer.cs
+++ Cantina/frmMenuPrincipal.Designer.cs
@@ -31,24 +31,121 @@ namespace Cantina
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenuPrincipal));
             this.btnVoltar = new System.Windows.Forms.Button();
+            this.btnFuncionarios = new System.Windows.Forms.Button();
+            this.btnClientes = new System.Windows.Forms.Button();
+            this.btnProdutos = new System.Windows.Forms.Button();
+            this.btnFornecedores = new System.Windows.Forms.Button();
+            this.btnConfiguracoes = new System.Windows.Forms.Button();
+            this.btnRelatorios = new System.Windows.Forms.Button();
+            this.btnVendas = new System.Windows.Forms.Button();
+            this.btnUsuarios = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnVoltar
             // 
             this.btnVoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnVoltar.Location = new System.Drawing.Point(599, 354);
+            this.btnVoltar.Location = new System.Drawing.Point(627, 405);
             this.btnVoltar.Name = "btnVoltar";
-            this.btnVoltar.Size = new System.Drawing.Size(167, 64);
+            this.btnVoltar.Size = new System.Drawing.Size(149, 38);
             this.btnVoltar.TabIndex = 0;
             this.btnVoltar.Text = "&Voltar";
             this.btnVoltar.UseVisualStyleBackColor = true;
             this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            // 
+            // btnFuncionarios
+            // 
+            this.btnFuncionarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFuncionarios.Image = ((System.Drawing.Image)(resources.GetObject("btnFuncionarios.Image")));
+            this.btnFuncionarios.Location = new System.Drawing.Point(34, 15);
+            this.btnFuncionarios.Name = "btnFuncionarios";
+            this.btnFuncionarios.Size = new System.Drawing.Size(150, 180);
+            this.btnFuncionarios.TabIndex = 1;
+            this.btnFuncionarios.Text = "&Funcionários";
+            this.btnFuncionarios.UseVisualStyleBackColor = true;
+            // 
+            // btnClientes
+            // 
+            this.btnClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClientes.Location = new System.Drawing.Point(226, 15);
+            this.btnClientes.Name = "btnClientes";
+            this.btnClientes.Size = new System.Drawing.Size(150, 180);
+            this.btnClientes.TabIndex = 2;
+            this.btnClientes.Text = "&Clientes";
+            this.btnClientes.UseVisualStyleBackColor = true;
+            // 
+            // btnProdutos
+            // 
+            this.btnProdutos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProdutos.Location = new System.Drawing.Point(428, 15);
+            this.btnProdutos.Name = "btnProdutos";
+            this.btnProdutos.Size = new System.Drawing.Size(150, 180);
+            this.btnProdutos.TabIndex = 3;
+            this.btnProdutos.Text = "&Produtos";
+            this.btnProdutos.UseVisualStyleBackColor = true;
+            // 
+            // btnFornecedores
+            // 
+            this.btnFornecedores.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFornecedores.Location = new System.Drawing.Point(623, 15);
+            this.btnFornecedores.Name = "btnFornecedores";
+            this.btnFornecedores.Size = new System.Drawing.Size(150, 180);
+            this.btnFornecedores.TabIndex = 4;
+            this.btnFornecedores.Text = "&Fornecedores";
+            this.btnFornecedores.UseVisualStyleBackColor = true;
+            // 
+            // btnConfiguracoes
+            // 
+            this.btnConfiguracoes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfiguracoes.Location = new System.Drawing.Point(623, 212);
+            this.btnConfiguracoes.Name = "btnConfiguracoes";
+            this.btnConfiguracoes.Size = new System.Drawing.Size(150, 180);
+            this.btnConfiguracoes.TabIndex = 8;
+            this.btnConfiguracoes.Text = "&Configurações";
+            this.btnConfiguracoes.UseVisualStyleBackColor = true;
+            // 
+            // btnRelatorios
+            // 
+            this.btnRelatorios.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRelatorios.Location = new System.Drawing.Point(428, 212);
+            this.btnRelatorios.Name = "btnRelatorios";
+            this.btnRelatorios.Size = new System.Drawing.Size(150, 180);
+            this.btnRelatorios.TabIndex = 7;
+            this.btnRelatorios.Text = "&Relatórios";
+            this.btnRelatorios.UseVisualStyleBackColor = true;
+            // 
+            // btnVendas
+            // 
+            this.btnVendas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVendas.Location = new System.Drawing.Point(226, 212);
+            this.btnVendas.Name = "btnVendas";
+            this.btnVendas.Size = new System.Drawing.Size(150, 180);
+            this.btnVendas.TabIndex = 6;
+            this.btnVendas.Text = "&Vendas";
+            this.btnVendas.UseVisualStyleBackColor = true;
+            // 
+            // btnUsuarios
+            // 
+            this.btnUsuarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUsuarios.Location = new System.Drawing.Point(34, 212);
+            this.btnUsuarios.Name = "btnUsuarios";
+            this.btnUsuarios.Size = new System.Drawing.Size(150, 180);
+            this.btnUsuarios.TabIndex = 5;
+            this.btnUsuarios.Text = "&Usuários";
+            this.btnUsuarios.UseVisualStyleBackColor = true;
             // 
             // frmMenuPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConfiguracoes);
+            this.Controls.Add(this.btnRelatorios);
+            this.Controls.Add(this.btnVendas);
+            this.Controls.Add(this.btnUsuarios);
+            this.Controls.Add(this.btnFornecedores);
+            this.Controls.Add(this.btnProdutos);
+            this.Controls.Add(this.btnClientes);
+            this.Controls.Add(this.btnFuncionarios);
             this.Controls.Add(this.btnVoltar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -64,5 +161,13 @@ namespace Cantina
         #endregion
 
         private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Button btnFuncionarios;
+        private System.Windows.Forms.Button btnClientes;
+        private System.Windows.Forms.Button btnProdutos;
+        private System.Windows.Forms.Button btnFornecedores;
+        private System.Windows.Forms.Button btnConfiguracoes;
+        private System.Windows.Forms.Button btnRelatorios;
+        private System.Windows.Forms.Button btnVendas;
+        private System.Windows.Forms.Button btnUsuarios;
     }
 }

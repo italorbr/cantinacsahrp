56f86b7d58c838b21bea27b30467e226d7bf5adf
lndsilva <lndsilva@hotmail.com>
2024-08-30 22:05:25 -0300

    funcionarios botões com imagens

--- Cantina/frmFuncionarios.Designer.cs
+++ Cantina/frmFuncionarios.Designer.cs
@@ -57,64 +57,82 @@ namespace Cantina
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(6, 10);
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(6, 9);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(102, 40);
+            this.button1.Size = new System.Drawing.Size(102, 42);
             this.button1.TabIndex = 0;
             this.button1.Text = "&Novo";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(119, 10);
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(119, 9);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(102, 40);
+            this.button2.Size = new System.Drawing.Size(102, 42);
             this.button2.TabIndex = 1;
             this.button2.Text = "&Cadastrar";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(232, 10);
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(232, 9);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(102, 40);
+            this.button3.Size = new System.Drawing.Size(102, 42);
             this.button3.TabIndex = 2;
             this.button3.Text = "&Alterar";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(344, 10);
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(344, 9);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(102, 40);
+            this.button4.Size = new System.Drawing.Size(102, 42);
             this.button4.TabIndex = 3;
             this.button4.Text = "&Excluir";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(456, 10);
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Location = new System.Drawing.Point(456, 9);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(102, 40);
+            this.button5.Size = new System.Drawing.Size(102, 42);
             this.button5.TabIndex = 4;
             this.button5.Text = "&Pesquisar";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(565, 10);
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.Location = new System.Drawing.Point(565, 9);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(102, 40);
+            this.button6.Size = new System.Drawing.Size(102, 42);
             this.button6.TabIndex = 5;
             this.button6.Text = "&Limpar";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button6.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button6.UseVisualStyleBackColor = true;
             // 
             // button7
             // 
             this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
-            this.button7.Location = new System.Drawing.Point(676, 10);
+            this.button7.Location = new System.Drawing.Point(676, 9);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(102, 40);
+            this.button7.Size = new System.Drawing.Size(102, 42);
             this.button7.TabIndex = 6;
             this.button7.Text = "&Voltar";
             this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
